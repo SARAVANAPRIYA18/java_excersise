6650788c0cf287ad468cd5270985f7ddfbaa6066
SARAVANAPRIYA18 <saravana9718@gmail.com>
2018-06-07 15:29:21 +0530

    Importing Class

---
+++ src/com/basic/examples/Vehicle.java
@@ -0,0 +1,10 @@
+package com.basic.examples;
+
+import com.training.examples.Car;
+import static java.lang.Math.PI;
+public class Vehicle {
+	public void brake(Car car) {
+		System.out.println("Examples of Import");
+		double d=PI;
+	}
+}
